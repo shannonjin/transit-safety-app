7c36092bd29100911e8059cbe8263415f958a44d
Shannon Jin <shannonj112@gmail.com>
2020-07-07 22:18:53 -0400

    Initial Commit

---
+++ transit safety app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  transit safety app
+//
+//  Created by Shannon Jin on 7/7/20.
+//  Copyright © 2020 Shannon Jin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ transit safety appTests/transit_safety_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  transit_safety_appTests.swift
+//  transit safety appTests
+//
+//  Created by Shannon Jin on 7/7/20.
+//  Copyright © 2020 Shannon Jin. All rights reserved.
+//
+
+import XCTest
+@testable import transit_safety_app
+
+class transit_safety_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
