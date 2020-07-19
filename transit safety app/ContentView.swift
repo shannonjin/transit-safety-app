327650502bd7ef244efeb2e7721d628135123254
OllieHarman97 <oh2212@columbia.edu>
2020-07-19 01:00:40 +0100

    Test

--- transit safety app/ContentView.swift
+++ transit safety app/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         
         VStack(alignment: .leading){
-            TextField("From", text: $startAddress)
+            TextField("Frick", text: $startAddress)
             .textFieldStyle(RoundedBorderTextFieldStyle())
             TextField("To", text: $destinationAddress)
             .textFieldStyle(RoundedBorderTextFieldStyle())
